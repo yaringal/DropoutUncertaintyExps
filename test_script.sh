6575fd6fcb9454817b5f3d2dccc0f36c87c93697
Jishnu Mukhoti <omegafragger@gmail.com>
2018-07-27 11:46:52 +0000

    Added the results obtained on all datasets without using ensembling

--- test_script.sh
+++ test_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-#python experiment.py --dir concrete -e 100 -nh 1
-#python experiment.py --dir energy -e 100 -nh 1
-#python experiment.py --dir kin8nm -e 100 -nh 1
-python experiment.py --dir power-plant -e 100 -nh 1
-python experiment.py --dir yacht -e 100 -nh 1
-python experiment.py --dir protein-tertiary-structure -e 100 -nh 1
-python experiment.py --dir naval-propulsion-plant -e 100 -nh 1
-python experiment.py --dir wine-quality-red -e 100 -nh 1
+python experiment_simple.py --dir bostonHousing -e 100 -nh 1
+python experiment_simple.py --dir concrete -e 100 -nh 1
+python experiment_simple.py --dir energy -e 100 -nh 1
+python experiment_simple.py --dir kin8nm -e 100 -nh 1
+python experiment_simple.py --dir power-plant -e 100 -nh 1
+python experiment_simple.py --dir yacht -e 100 -nh 1
+python experiment_simple.py --dir protein-tertiary-structure -e 100 -nh 1
+python experiment_simple.py --dir naval-propulsion-plant -e 100 -nh 1
+python experiment_simple.py --dir wine-quality-red -e 100 -nh 1
